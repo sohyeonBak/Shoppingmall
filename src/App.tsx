9b5bc58414ccd601a61aecde5d2825d04afe283b
sohyeonBak <sohyeonbakoly@gmail.com>
2021-08-16 23:13:35 +0900

    init setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Stack from "./component/admin/stack";
 import Header from "./component/header/header"
 import Home from "./component/home/home"
 import Login from "./component/login/login";
-import Signup from "./component/signup/signup";
+import MyInfo from "./component/mypage/myinfo";
+import ShoppingBasket from "./component/shopping-basket/shopping-basket";
+import './style/scss/App.scss';
 
 function App() {
   return (
@@ -15,8 +18,14 @@ function App() {
         <Route path='/login' >
           <Login />
         </Route>
-        <Route path='/signup' >
-          <Signup />
+        <Route path='/myshop' >
+          <ShoppingBasket />
+        </Route>
+        <Route path='/mypage' >
+          <MyInfo />
+        </Route>
+        <Route path='/admin' >
+          <Stack />
         </Route>
       </Switch>
     </BrowserRouter>
--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -1,20 +1,32 @@
-
 import { Link } from 'react-router-dom';
+import '../../style/scss/header.scss'
 
 const Header = () => {
   return(
     <header>
-      <h1>
-        <Link to={'/'}>HOME</Link>
-      </h1>
-      <ul>
-        <li>
-          <Link to={'/login'}>login</Link>
-        </li>
-        <li>
-          <Link to={'/signup'}>signup</Link>
-        </li>
-      </ul>
+      <div className="site-header">
+        <ul className="detail">
+          <li>카테고리</li>
+          <li>검색</li>
+        </ul>
+        <div className="logo">
+          <h1>
+            <Link to={'/'}>HOME</Link>
+          </h1>
+        </div>
+        <ul className="user">
+          <li>
+            <Link to={'/login'}>login</Link>
+          </li>
+          <li>
+            <Link to = {'/myshop'}>장바구니</Link>
+          </li>
+          <li>
+            <Link to = {'/mypage'}>My Page</Link>
+          </li>
+        </ul>
+      </div>
+      
     </header>
   )
 }
